47839abcb4bfcc8259ffa6712ffb5dd666b740cb
Sakakura <mcd2376090@stu.o-hara.ac.jp>
2024-11-22 16:51:28 +0900

    inputAction追加

--- Assets/Sakakura/Scripts/Player.cs
+++ Assets/Sakakura/Scripts/Player.cs
@@ -2,28 +2,30 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private int nowCharaNum = 0;
 
-    [SerializeField] private GameObject[] _playerModel;
-    [SerializeField] CinemachineStateDrivenCamera _stateCam;
-    CinemachineFreeLook _freeLookCam;
+    [SerializeField] private List<GameObject> _playerList;
+    [SerializeField] private CinemachineStateDrivenCamera _stateCam;
+    private CinemachineFreeLook _freeLookCam;
+    private bool _canParry;
 
     private Animator _animator;
 
     void Start()
     {
-        for (int i = 0; i < _playerModel.Length; i++)
+        for (int i = 0; i < _playerList.Count; i++)
         {
             if (i == nowCharaNum)
             {
-                _playerModel[i].gameObject.SetActive(true);
+                _playerList[i].gameObject.SetActive(true);
                 AttachPlayer(i);
             }
             else
-                _playerModel[i].gameObject.SetActive(false);
+                _playerList[i].gameObject.SetActive(false);
         }
 
         _freeLookCam = _stateCam.GetComponentInChildren<CinemachineFreeLook>();
@@ -31,58 +33,75 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            // キャラチェンジ
-            CharaChange(-1, -_playerModel[nowCharaNum].transform.forward * 2.0f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            // キャラチェンジ
-            CharaChange(1, -_playerModel[nowCharaNum].transform.forward * 2.0f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // キャラチェンジ
-            CharaChange(1, Vector3.zero);
-            _animator.SetTrigger("Parry");
-            // 通常カメラをリセット
-            _freeLookCam.m_XAxis.Value = _playerModel[nowCharaNum].transform.eulerAngles.y;
-            _freeLookCam.m_YAxis.Value = 0.5f;
-        }
+        // 体力チェック
+        CheckDeadCharacter(1);
     }
 
     void CharaChange(int charaDir, Vector3 entryOffset)
     {
         // 次の登場位置を決める
-        Vector3 playerPos = _playerModel[nowCharaNum].transform.position;
-        Vector3 rotate = _playerModel[nowCharaNum].transform.localEulerAngles;
+        Vector3 playerPos = _playerList[nowCharaNum].transform.position;
+        Vector3 rotate = _playerList[nowCharaNum].transform.localEulerAngles;
 
         // 今のキャラを消す
-        _playerModel[nowCharaNum].SetActive(false);
+        _playerList[nowCharaNum].SetActive(false);
 
         // 今のキャラの番号を変更
-        nowCharaNum += charaDir;
-        if (nowCharaNum < 0)
-            nowCharaNum = _playerModel.Length - 1;
-        else if (nowCharaNum >= _playerModel.Length)
-            nowCharaNum = 0;
+        ChangeCharaNum(charaDir);
 
         // 新しいキャラに切り替える
-        _playerModel[nowCharaNum].SetActive(true);
-        _playerModel[nowCharaNum].transform.position = playerPos + entryOffset;
-        _playerModel[nowCharaNum].transform.localEulerAngles = rotate;
+        _playerList[nowCharaNum].SetActive(true);
+        _playerList[nowCharaNum].transform.position = playerPos + entryOffset;
+        _playerList[nowCharaNum].transform.localEulerAngles = rotate;
         AttachPlayer(nowCharaNum);
         _animator.SetTrigger("TransFrontline");
     }
 
     void AttachPlayer(int playerNum)
     {
-        _animator = _playerModel[nowCharaNum].transform.GetComponent<Animator>();
-        _stateCam.Follow = _playerModel[playerNum].transform;
-        _stateCam.LookAt = _playerModel[playerNum].transform;
+        _animator = _playerList[playerNum].transform.GetComponent<Animator>();
+        _stateCam.Follow = _playerList[playerNum].transform;
+        _stateCam.LookAt = _playerList[playerNum].transform;
         _stateCam.m_AnimatedTarget = _animator;
+    }
+
+    void CheckDeadCharacter(float health)
+    {
+        if (health <= 0)
+        {
+            _playerList.RemoveAt(nowCharaNum);
+
+            ChangeCharaNum(1);
+        }
+    }
+
+    void ChangeCharaNum(int charaDir)
+    {
+        // 今のキャラの番号を変更
+        nowCharaNum += charaDir;
+        if (nowCharaNum < 0)
+            nowCharaNum = _playerList.Count - 1;
+        else if (nowCharaNum >= _playerList.Count)
+            nowCharaNum = 0;
+    }
+
+    public void OnChangeNextChara()
+    {
+        CharaChange(1, -_playerList[nowCharaNum].transform.forward * 2.0f);
+    }
+
+    public void OnChangeBeforeChara()
+    {
+        CharaChange(-1, -_playerList[nowCharaNum].transform.forward * 2.0f);
+    }
+
+    public void OnParry()
+    {
+        // キャラチェンジ
+        CharaChange(1, Vector3.zero);
+        _animator.SetTrigger("Parry");
+        // 通常カメラをリセット
+        _freeLookCam.m_XAxis.Value = _playerList[nowCharaNum].transform.eulerAngles.y;
+        _freeLookCam.m_YAxis.Value = 0.5f;
     }
 }
