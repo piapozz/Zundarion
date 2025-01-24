6b7e21c7eda8b9f09e7982dd1d7f8017b28c2f26
Sein <mcd2376075@stu.o-hara.ac.jp>
2025-01-24 10:48:33 +0900

    敵のステートを修正 ・正しく実装されていない部分を実装しなおしました。

--- Assets/Scripts/Characters/Enemy/MainEnemy/EnemyBear.cs
+++ Assets/Scripts/Characters/Enemy/MainEnemy/EnemyBear.cs
@@ -14,43 +14,19 @@ public class EnemyBear : BaseEnemy
 {
     float distance;
 
-    Quaternion rotation;
+    Quaternion targetRotation;
 
     private void Update()
     {
-        enemyForward = Vector3.forward;
-        relativePosition = GetRelativePosition();
-
-        // プレイヤーの方を向く
-        rotation = LookAtMe();
-        SetRotation(rotation);
-
-        distance = DistanceToPlayer();
-
-        if (!selfAnimator.GetBool("Found"))
-        {
-            if (distance <= 5.0f)
-            {
-                SetAnimatorBool("Found", true);
-                SetAnimatorBool("Running", true);
-            }
-        }
-
-        else
-        {
-            if (distance <= 3.0f)
-            {
-                SetAnimatorTrigger("Attack");
-            }
-        }
-
-
-
+        SetRotation(targetRotation);
+        // 距離を取得
+        distance = GetRelativePosition().magnitude;
+        Debug.Log(distance);
     }
 
     public override void Attack()
     {
-        
+
     }
 
     public override void StrongAttack()
@@ -59,6 +35,66 @@ public class EnemyBear : BaseEnemy
     }
 
     public override void UniqueAttack()
+    {
+
+    }
+
+    public override void Restraint()
+    {
+        // 追跡範囲よりも離れていたら
+        if (distance >= 15.0f)
+        {
+            // 見失う
+            SetAnimatorBool("Found", false);
+            SetAnimatorBool("Restraint", false);
+            return;
+        }
+
+        // 弱攻撃か強攻撃をする
+        if (distance <= 3.0f)
+        {
+            int attackType = Random.Range(0, 2);
+            if (attackType == 0) SetAnimatorTrigger("Attack");
+            if (attackType == 1) SetAnimatorTrigger("StrongAttack");
+        }
+        // ジャンプ攻撃に遷移
+        else if (distance >= 6.0f && distance <= 10.0f)
+        {
+            SetAnimatorTrigger("UniqueAttack");
+        }
+        // 追跡に遷移
+        else
+        {
+            SetAnimatorBool("Chasing", true);
+        }
+    }
+
+    public override void Wandering()
+    {
+        // 発見
+        if (distance <= 7.0f) SetAnimatorBool("Found", true);
+    }
+
+    public override void Found()
+    {
+
+    }
+
+    public override void Chasing()
+    {
+        if (distance <= 3.0f) SetAnimatorBool("Chasing", false);
+
+        Transform enemyTransform = transform;
+        Transform playerTransform = player.transform;
+
+        // プレイヤーの方向を計算
+        Vector3 directionToPlayer = (playerTransform.position - enemyTransform.position).normalized;
+
+        // プレイヤーの方向を向く
+        targetRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
+    }
+
+    public override void Dying()
     {
 
     }
