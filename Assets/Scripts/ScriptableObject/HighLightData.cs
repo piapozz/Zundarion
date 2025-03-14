ec3f9e6bec7f21385eda9991a4e304b79f18d23e
Sein <mcd2376075@stu.o-hara.ac.jp>
2025-03-14 14:37:28 +0900

    敵の予兆をOutlineで表現・その他 ・シーンのボタンに初期設定を追加 ・敵の予兆を変更

---
+++ Assets/Scripts/ScriptableObject/HighLightData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "HighLightData", menuName = "ScriptableObjects/HighLightData")]
+public class HighLightData : ScriptableObject
+{
+    public Color highLightColor;
+    public float highLightSecond;
+}
